8f650714749b5f9a2fbcdf9c76e310500eab6065
bluessista <inga@bluessista.com>
2018-03-31 00:20:07 +0200

    add cardhandler and js for winners popup

--- _phase2/Memory_Game/js/app.js
+++ _phase2/Memory_Game/js/app.js
@@ -5,13 +5,13 @@ var icons = [
     'bomb', 'bomb', 
     'diamond', 'diamond', 
     'bicycle', 'bicycle', 
-    'earlybirds', 'earlybirds', 
+    'hand-spock-o', 'hand-spock-o', 
     'bug', 'bug',
     'beer', 'beer',
     'anchor', 'anchor',
     'android', 'android'
 ];
-var opened = []; //stores the cards later
+var opened = []; //stores the cards
 var match = 0;
 var $scorePanel = document.querySelector('.score-panel');
 var moves = 0;
@@ -21,10 +21,10 @@ var second = 0;
 var timer = document.querySelector('.time');
 var ratingStars = document.querySelector('.fa-star');
 var gameboard = document.querySelector('.gameboard');
-var card = document.querySelector('.card');
 var stars3 = 10;
 var stars2 = 18;
 var star1 = 30;
+var allCardsOpen = icons.length / 2;
 var successDelay = 400;
 var reset = document.querySelector('.restart');
 
@@ -37,7 +37,6 @@ var reset = document.querySelector('.restart');
 var init = () => {
     var cards = shuffle(icons);
     generateCards(icons);
-
     // gameboard.innerHTML = '';
     // match = 0;
     // moves = 0;
@@ -46,7 +45,7 @@ var init = () => {
     // second = 0;
     timer.innerHTML = (`${second}`);
     startTimer();
-
+    addCardListener();
 }
 
 // Shuffle function from http://stackoverflow.com/a/2450976
@@ -67,9 +66,9 @@ function shuffle(array) {
 //Generate all Cards with shuffled Icons
 const generateCards = (array) => {
     for(var i = 0; i < array.length; i++) {
-        var cardLi = gameboard.appendChild(document.createElement('li'));
-        cardLi.classList.add('card');
-        cardLi.innerHTML = '<i class="fa fa-'+icons[i]+'"></i>';
+        var card = gameboard.appendChild(document.createElement('li'));
+        card.classList.add('card');
+        card.innerHTML = '<i class="fa fa-'+icons[i]+'"></i>';
     };
 }
 
@@ -89,7 +88,114 @@ const handleSeconds = () => {
     second = second + 1;
 }
 
+//Handle Rating
+const handleRating = (moves) => {
+    var rates = 3;
+    if(moves > stars3 && moves < stars2) {
+        ratingStars.classList.toggle('fa-star-o');
+        rating = 2;
+    } else if (moves > stars2 && moves < star1) {
+        ratingStars.classList.toggle('fa-star-o');
+        rating = 1;
+    } else {
+        ratingStars.classList.toggle('fa-star-o');
+        rating = 0;
+    }
+    return { score: rating};
+}
 
+// Handle Card Moves
+const addCardListener = () => {
+    var cards = gameboard.querySelectorAll('.card');
+    cards.forEach(handleCards);
+
+    if (allCardsOpen === match) {
+        handleRating(moves);
+        var score = handleRating(moves).score;
+        setTimeout(function () {
+            winningPopUp(moves, score);
+        }, 500);
+    }
+};
+
+const handleCards = (card) => {
+    card.addEventListener('click', function () {
+        if(this.hasAttribute('show') || this.hasAttribute('match')) {
+            return true;
+        }
+        var openCard = this.innerHTML;
+        this.classList.add('open', 'show');
+        opened.push(openCard);
+        console.log(this);
+
+        if(opened.length > 1) {
+            if(openCard === opened[0]) {
+                var cardsOpen = gameboard.querySelectorAll('.open');
+
+                cardsOpen.forEach((el) => {
+                    el.classList.add('match');
+                });
+
+                setTimeout(function() {
+                    var cardsMatch = gameboard.querySelectorAll('.match');
+                    cardsMatch.forEach((el) => {
+                        el.classList.remove('open', 'show');
+                    });
+                }, successDelay);
+                match++;
+            } else {
+                var cardsOpen = gameboard.querySelectorAll('.open');
+                cardsOpen.forEach((el) => {
+                    el.classList.remove('nomatch');
+                    setTimeout(function() {
+                        el.classList.remove('open', 'show');
+                    }, successDelay);
+                });
+            }
+            opened = [];
+            moves++;
+            handleRating(moves);
+            moveCount.innerHTML = moves;
+        }
+    });
+};
+
+// Game Winner Popup Box via Sweetalert2.js
+const winningPopUp = (moves, moveCount) => {
+    swal({
+		allowEscapeKey: false,
+		allowOutsideClick: false,
+		title: 'Congratulations! You Won!',
+		text: 'With ' + moves + ' moves and ' + moveCount + ' Stars in ' + second + ' Seconds.\n Woooooo!',
+		type: 'success',
+		confirmButtonColor: '#7a43a4',
+		confirmButtonText: 'Play again!'
+	}).then(function (isConfirm) {
+		if (isConfirm) {
+			initGame();
+		}
+	})
+}
+
+//Restart Game Popup via Sweetalert2.js
+const restartingPopUp = () => {
+    swal({
+		allowEscapeKey: false,
+		allowOutsideClick: false,
+		title: "You're tough!",
+		text: "Are you sure you wanna play again?",
+		type: 'warning',
+		showCancelButton: true,
+		confirmButtonColor: '#7a43a4',
+		cancelButtonColor: '#f95c3c',
+		confirmButtonText: 'Yes, Mam',
+	}).then(function (isConfirm) {
+		if (isConfirm) {
+			initGame();
+		}
+	})
+}
+reset.addEventListener('click', restartingPopUp);
 
 init();
 
